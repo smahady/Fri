8dfe86d46affbe92c9ab12964f9ffc0517b82776
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-08-07 00:53:59 +0000

    .

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import QTimer
 import sys
 
 # Scene has now INHERITED QWidget
 # Scene IS A QWidget
 class Scene (QWidget) :
-	def __init__(self, x=600, y=400, speed=200,):
+	def __init__(self, x=600, y=400, speed=200):
 		super().__init__()
 		self.setGeometry(50,50,x,y)
+		self.timer=QTimer()
+		self.timer.timeout.connect(self.updateTask)
+		self.speed = speed
+
 	
 	def start(self):
-		pass
+		self.timer.start(self.speed)
+		
+
 	def stop(self):
+		self.timer.stop()
+
+	def updateTask(self):
+		self.update()
+
+	def paintEvent(self.event):
+		self.updateGame()
+
+	def updateGame(self):
 		pass
+		
+
+class Game(Scene):
+	def __init__(self):
+		super().__init__()
+	
+	def updateGame(self):
+		print("My Update")
+	
+
 
 app = QApplication(sys.argv)
-scene = Scene()
+scene = Game()
 scene.show()
 sys.exit(app.exec_())
