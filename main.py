8f01d07ac2e373319de14e8259538fcb00f28a5d
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-08-14 00:33:20 +0000

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtCore import QTimer
 import sys
+from Sprite import Sprite
 
 # Scene has now INHERITED QWidget
 # Scene IS A QWidget
@@ -33,13 +34,16 @@ class Scene (QWidget) :
 class Game(Scene):
 	def __init__(self):
 		super().__init__()
+    # Sprite.__init__(self, thisScene, imageFile, xSize, ySize)
+		self.sprite = Sprite(self, "spaceship100.png", 100,93)
 	
 	def updateGame(self):
 		print("My Update")
 	
 
 
-app = QApplication(sys.argv)
+
+app = QApplication(sys.argv) #QApplication.__init__(self, sys.argv)
 scene = Game()
 scene.show()
 sys.exit(app.exec_())
